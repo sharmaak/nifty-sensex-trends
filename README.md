46c0ea5bf9651411b791b4a6bf231b162b516cc1
Amit Kumar Sharma <amit.official@gmail.com>
2024-09-03 01:58:50 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,49 @@
+## Intro 
+Nifty Sensex Trends is a very simple and basic code to find average points nifty and sensex
+gained for last 2y, 5y, 10y and max. 
+
+The author 
+
+TL;DR: 
+Just look at nifty.txt and sensex.txt for results. 
+
+## Disclaimer
+
+The code provided is for informational and educational purposes only. The author is not a financial advisor and does not hold any responsibility for any investment decisions or trades made based on the use of this code. Users should perform their own research and consult with a qualified financial advisor before making any investment decisions. The author will not be liable for any financial losses or damages arising from the use of this code.
+
+## Project Setup 
+
+### Windows 11
+```powershell
+# Refer to https://github.com/pyenv-win/pyenv-win?tab=readme-ov-file#quick-start
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
+# Restart powershell and run the following command to verify
+pyenv --version
+pyenv install 3.10.14
+# Set local python version
+pyenv local 3.10.14
+python -m venv venv
+.\venv\Scripts\Activate.ps1
+pip install -r requirements.txt
+```
+
+### Mac
+```bash
+brew install pyenv
+pyenv install 3.10.14
+pyenv local 3.10.14
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+### Linux 
+```bash
+# https://stackoverflow.com/questions/77550543/ubuntu-22-04-2-command-pyenv-not-found
+curl https://pyenv.run | bash
+pyenv install 3.10.14
+pyenv local 3.10.14
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
---
+++ nifty_sensex.py
@@ -0,0 +1,51 @@
+import yfinance as yf
+from pprint import pprint
+
+def dig(index: str, duration_in_years: int):
+	
+	period = 'max' if duration_in_years <= 0 else f'{duration_in_years}y'
+	data = yf.download(index, interval='1mo', period=period, progress=False)
+	print(f"Period: {period}, Start Date: {data.index[0].strftime('%d-%m-%y')}, End Date: {data.index[len(data)-1].strftime('%d-%m-%y')}")
+
+	# 1. Calculate monthly average gain in points and percentage
+	data['Monthly_Gain'] = data['Close'] - data['Open']
+	average_monthly_gain = data['Monthly_Gain'].mean()
+	print(f"average_monthly_gain: {round(average_monthly_gain,2)}")
+
+	# 2. Now we want to find that on an average for each month, what was the average gain across years? 
+	book = {}
+	for i in range(1,13):
+		book.update({"{:02d}".format(i): []})
+
+	for i in range(0, len(data)):
+		month = data.index[i].strftime('%m')
+		gain = round(float(data['Monthly_Gain'].iloc[i]), 2)
+		book[month].append(gain)
+
+	print('Month, Loss Ratio, Average Gain')
+	for m in book.keys():
+		month_gain_values = book[m]
+		
+		average_gain = sum(month_gain_values) / len(month_gain_values)
+
+		gain_count = 0
+		loss_count = 0
+		for v in month_gain_values:
+			if v > 0: 
+				gain_count = gain_count + 1
+			else:
+				loss_count = loss_count + 1
+		profit_ratio = gain_count / (gain_count + loss_count)
+		print(f"{m}, {loss_count}/{(gain_count + loss_count)}, {round(average_gain, 2)}")
+	print('-----------------')
+
+
+if __name__ == '__main__':
+	# period must be one of ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']")
+	BSE='^BSESN'
+	NSE='^NSEI'
+	index=BSE
+	dig(index, 2)
+	dig(index, 5)
+	dig(index, 10)
+	dig(index, -1)
